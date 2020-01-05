448964e91588349eb71896e48260dda68081ed33
Ravi Pabari <46424422+RaviPabari@users.noreply.github.com>
2020-01-05 21:17:18 +0530

    Add files via upload

---
+++ 1) Popup Menu/menu.dart
@@ -0,0 +1,11 @@
+class Menu{
+  static const String Subscribe = 'About';
+  static const String Logout = 'LogOut';
+  static const String Settings = 'Settings';
+
+  static const List<String> choices  = <String>[
+    Subscribe,
+    Logout,
+    Settings
+  ];
+}
